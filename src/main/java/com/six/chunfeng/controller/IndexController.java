e3b59ed4d5c96799166f8bd3ea2497fa234f60f1
yyf <yyf@DESKTOP-7G22C3V>
2019-03-06 16:31:23 +0800

    test

--- src/main/java/com/six/chunfeng/controller/IndexController.java
+++ src/main/java/com/six/chunfeng/controller/IndexController.java
@@ -1,14 +1,20 @@
 package com.six.chunfeng.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.six.chunfeng.service.IndexService;
 
-@RestController
+@Controller
 public class IndexController {
 
 	@Autowired
 	private IndexService indexService;
 	
+	@GetMapping("/index")
+	public String index(){
+		return "Home.html";
+	}
 }
