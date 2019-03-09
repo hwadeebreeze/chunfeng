89b47268ac704342ccfa0da6cf65170ff05acc24
yyf <yyf@DESKTOP-7G22C3V.lab.hwadee.com>
2019-03-09 16:21:40 +0800

    add commend head

--- src/main/java/com/six/chunfeng/service/IndexService.java
+++ src/main/java/com/six/chunfeng/service/IndexService.java
@@ -1,8 +1,22 @@
 package com.six.chunfeng.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.six.chunfeng.domain.JobInfo;
 
 @Service
 public class IndexService {
-
+	List<JobInfo> list;
+	
+	public void recommend(Integer id){
+		
+	}
+	
+	public List<JobInfo> changePage(Integer curPage){
+		return null;
+	}
+	
+	
 }
