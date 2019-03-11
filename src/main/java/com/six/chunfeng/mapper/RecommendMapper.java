5684097cace41d8b02cc599cb3b6c023110b29e1
QZ <1578166061@qq.com>
2019-03-11 10:17:25 +0800

    1. 为项目添加热启动依赖, 2. 添加推荐服务类,添加推荐mapper.

---
+++ src/main/java/com/six/chunfeng/mapper/RecommendMapper.java
@@ -0,0 +1,18 @@
+package com.six.chunfeng.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.six.chunfeng.domain.User;
+
+public interface RecommendMapper {
+
+	@Select("select pswd from user_info where tel=#{phone}")
+	public String getPassworkByphone(@Param("phone") String phone);
+	
+	@Select("select * from user_info where user_id=#{id}")
+	public User getUserById(int id);
+	
+	@Select("select user_id from user_info where tel=#{phone}")
+	public Integer getIdByPhone(String phone);
+}
--- src/main/java/com/six/chunfeng/service/RecommendService.java
+++ src/main/java/com/six/chunfeng/service/RecommendService.java
@@ -1,22 +1,29 @@
 package com.six.chunfeng.service;
 
 import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.six.chunfeng.domain.JobInfo;
+import com.six.chunfeng.mapper.InfoMapper;
 
 @Service
 public class RecommendService {
 	
 	List<JobInfo> list;
-
-	public void recommend(Integer id){
-		//noble
-
+	
+	@Autowired
+	private InfoMapper infoMapper;
+	public void recommend(int id)
+	{
+		JobInfo info = infoMapper.getJobInfoById(id);
+		System.out.println(info);
 	}
 
-	public List<JobInfo> changePage(Integer curPage){
+	public List<JobInfo> changePage(Integer curPage)
+	{
 		return null;
 	}
+	
+
 }
